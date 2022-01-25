5ac2436c4a666f2516024f3a7ef3cc3d57239bde
jerome gerard <jerome.gerard02@outlook.fr>
2022-01-25 19:01:13 +0100

    seance 25/01

--- app/src/main/java/com/jger/groupe4/LastComputeActivity.java
+++ app/src/main/java/com/jger/groupe4/LastComputeActivity.java
@@ -7,6 +7,10 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class LastComputeActivity extends AppCompatActivity {
+    private Long premierElement = 0L;
+    private Long deuxiemeElement = 0L;
+    private String symbol;
+    private Long resultat = 0L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,12 @@ public class LastComputeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_last_compute);
         Button boutonPrecedent = findViewById(R.id.bouton_precedent);
         boutonPrecedent.setOnClickListener(view -> retourneAuPrecedent());
+        Intent intent =getIntent();
+        premierElement = intent.getLongExtra("premierElement",0);
+        deuxiemeElement = intent.getLongExtra("deuxiemeElement",0);
+        symbol = intent.getStringExtra("symbol");
+        resultat = intent.getLongExtra("resultat",0L);
+
     }
 
     private void retourneAuPrecedent() {
